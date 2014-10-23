c0feb61bd1ed96d252496ce13a7e6bdcf3329a99
verkholantsev <poshli.pismo@gmail.com>
2014-10-24 01:40:30 +0400

    Stylus added

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
 
-gulp.task('pages', function () {
-    gulp.src('./src/pages/**/*.jade')
+gulp.task('templates', function () {
+    gulp.src('./src/templates/**/*.jade')
         .pipe(jade().on('error', function (error) {
             console.log(error.toString());
         }))
         .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('default', function () {
-    gulp.watch('./**/*.jade', function () {
-        gulp.run('pages');
+gulp.task('styles', function () {
+    gulp.src('./src/styles/**/*.styl')
+        .pipe(stylus({
+            sourcemap: {
+                inline: true,
+                sourceRoot: '.',
+                basePath: './src/styles'
+            }
+        }).on('error', function (error) {
+            console.log(error.toString());
+        }))
+        .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./**/*.jade', './**/*.styl'], function () {
+        gulp.run('templates', 'styles');
     });
 });
